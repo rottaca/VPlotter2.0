80e9e7e53393a44511a381445d0585c98259a836
Andreas Rottach <andirottach@aol.com>
2018-06-23 12:18:20 +0200

    Added missed config file.

---
+++ plotter/config.py
@@ -0,0 +1,14 @@
+
+
+PLOTTER_HARDWARE_CONFIG={
+    "dir_pins"      : (0, 1),
+    "step_pins"     : (2, 3),
+    "res_pins"      : (2, 3, 4),
+    "res_type"      : "Full",
+    "servo_pin"     : 9,
+    "servo_pos_up"  : 3,
+    "servo_pos_down": 10,
+    "base_width"    : 1000,
+    "movement_resolution": 10,
+    "steps_per_mm": 200
+}
